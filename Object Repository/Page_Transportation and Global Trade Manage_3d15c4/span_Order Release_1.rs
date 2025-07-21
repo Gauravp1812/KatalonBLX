4cfdc88cf4d980ecc25473205ab45f41e2c6819c
GauravPrasad <GauravPrasad@192.168.1.34>
2025-07-21 17:58:27 +0530

    My 1st project

---
+++ Object Repository/Page_Transportation and Global Trade Manage_3d15c4/span_Order Release_1.rs
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Order Release_1</name>
+   <tag></tag>
+   <elementGuidId>daae7fff-fd98-4211-af35-7216452938aa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.oj-treeview-item-content.oj-hover > span.oj-treeview-item-text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@id='sb_0_2_0']/div/span[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=treeitem[name=&quot;Order Release&quot;s] >> internal:attr=[title=&quot;Order Release&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>280f8f15-2946-49e1-b212-c45c2a0b852d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>oj-treeview-item-text</value>
+      <webElementGuid>ddc5b429-967a-471e-bd14-6562a50400d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>:title</name>
+      <type>Main</type>
+      <value>[[row.data.text]]</value>
+      <webElementGuid>742240b9-6cdd-4238-a6ca-f3d0f98c55a9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>:style</name>
+      <type>Main</type>
+      <value>[[{'color':theme_springboard_submenu_fontcolor}]]</value>
+      <webElementGuid>f59a095a-25cf-4032-b5b3-26b88d787bd9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Order Release</value>
+      <webElementGuid>5d6a0ab7-5e88-4848-80ce-ca2387513139</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                      Order Release
+                    </value>
+      <webElementGuid>1cadb10f-041f-49b8-bf6f-72b321370aa3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;sb_0_2_0&quot;)/div[@class=&quot;oj-treeview-item-content oj-hover&quot;]/span[@class=&quot;oj-treeview-item-text&quot;]</value>
+      <webElementGuid>55637395-ca11-418c-8cb0-9c492dc52253</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='sb_0_2_0']/div/span[2]</value>
+      <webElementGuid>965c6443-43a2-42ab-a621-0da954f64371</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div/oj-tree-view/ul/li[3]/ul/li/div/span[2]</value>
+      <webElementGuid>a77c8b4c-3bb5-41da-9fe2-14375a38a74a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[@title = 'Order Release' and (text() = '&#xd;
+                      Order Release&#xd;
+                    ' or . = '&#xd;
+                      Order Release&#xd;
+                    ')]</value>
+      <webElementGuid>00e0a28c-9cfe-47a8-b199-8776c4ded77a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/OTM Test Suite/New Test Suite.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b2659af8-05da-42f0-b76c-6c6619b18780</testSuiteGuid>
+   <testCaseLink>
+      <guid>5d31b03c-b9c5-4e01-a9cb-cd9c06258668</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_362_P3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>196fbe16-5a3e-4604-ad04-65dfd3bd81b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_331_P3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
